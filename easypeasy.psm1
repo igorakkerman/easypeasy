d913eef4b68e6d0a9f1602aa7aecf87741c06765
Igor Akkerman <commit@igorakkerman.de>
2023-02-03 23:40:48 +0100

    Re-order includes

--- easypeasy.psm1
+++ easypeasy.psm1
@@ -1,7 +1,5 @@
-Write-Output "Welcome to easypeasy!"
-
-. "$PSScriptRoot\timestamp.ps1"
-. "$PSScriptRoot\specialfolder.ps1"
-. "$PSScriptRoot\explorer.ps1"
-. "$PSScriptRoot\systempath.ps1"
 . "$PSScriptRoot\environment.ps1"
+. "$PSScriptRoot\explorer.ps1"
+. "$PSScriptRoot\specialfolder.ps1"
+. "$PSScriptRoot\systempath.ps1"
+. "$PSScriptRoot\timestamp.ps1"
