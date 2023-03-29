a3684dbfe4429fca2f4302e63e974dc36626f5dd
Igor Akkerman <commit@igorakkerman.de>
2023-03-29 18:21:45 +0200

    Add "Path" as alias for ShortcutPath in Set-ShortcutRunAsAdministrator

--- shortcut.ps1
+++ shortcut.ps1
@@ -1,11 +1,29 @@
 function Set-ShortcutRunAsAdministrator {
+
+    <#
+    .SYNOPSIS
+        Modifies a shortcut to run as administrator.
+
+    .DESCRIPTION
+        Sets the "Run as administrator" flag on the specified shortcut.
+
+    .PARAMETER ShortcutPath
+        The path to the shortcut to set the "Run as administrator" flag on.
+
+    .EXAMPLE
+        Set-ShortcutRunAsAdministrator -ShortcutPath "C:\Users\UserName\Desktop\MyShortcut.lnk"
+
+    .NOTES
+        This function is based on the answer at https://stackoverflow.com/a/29002207/2562544.
+    #>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
+        [Alias("Path")]
         [string] $ShortcutPath
     )
 
-    # https://stackoverflow.com/a/29002207/2562544
     $bytes = [System.IO.File]::ReadAllBytes($ShortcutPath)
     $bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
     [System.IO.File]::WriteAllBytes($ShortcutPath, $bytes)
