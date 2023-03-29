a0bb630cf345bd81ea41bae8b08a6d1cde1d99ee
Igor Akkerman <commit@igorakkerman.de>
2023-03-29 18:29:20 +0200

    Add documentation to Stop-Explorer

--- explorer.ps1
+++ explorer.ps1
@@ -3,6 +3,18 @@ function Stop-Explorer {
     Param()
 
     Stop-Process -ProcessName explorer
+
+    <#
+    .SYNOPSIS
+        Restarts Windows Explorer.
+
+    .DESCRIPTION
+        Stops the Windows Explorer process, which (generally) triggers a restart.
+
+    .ALIASES
+        sx
+    #>
+
 }
 
 New-Alias -Name sx -Value Stop-Explorer
