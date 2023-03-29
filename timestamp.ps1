0f982303e400c0fda7afa2162589a6ebd2f6ca05
Igor Akkerman <commit@igorakkerman.de>
2023-03-29 19:16:00 +0200

    Add documentation to Get-Timestamp

--- timestamp.ps1
+++ timestamp.ps1
@@ -3,6 +3,17 @@ function Get-Timestamp {
     Param()
 
     Get-Date -Format "yyyy-MM-dd HH.mm.ss"
+
+    <#
+    .SYNOPSIS
+        Returns the current date and time.
+
+    .DESCRIPTION
+        Returns a timestamp of the current instant in the format "yyyy-MM-dd HH.mm.ss".
+
+    .ALIASES
+        time
+    #>
 }
 
 New-Alias -Name time -Value Get-Timestamp
