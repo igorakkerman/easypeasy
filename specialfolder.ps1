1bd8623cb7e7aa896a8dbe05a03cc33888e9175d
Igor Akkerman <commit@igorakkerman.de>
2023-03-29 18:41:49 +0200

    Add documentation to Get-MyDocumentsFolder

--- specialfolder.ps1
+++ specialfolder.ps1
@@ -1,6 +1,20 @@
 function Get-MyDocumentsFolder {
+    [CmdletBinding()]
+    Param()
+    
     $wshShell = New-Object -ComObject WScript.Shell
     return $wshShell.SpecialFolders("MyDocuments")
+
+    <#
+    .SYNOPSIS
+        Returns the path to the My Documents folder.
+
+    .DESCRIPTION
+        Returns the path to the My Documents folder.
+
+    .ALIASES
+        docs
+    #>
 }
 
 New-Alias -Name docs -Value Get-MyDocumentsFolder
