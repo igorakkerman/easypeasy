899079395c563a445addf4eed82dd1ab0cddc3ac
Igor Akkerman <commit@igorakkerman.de>
2023-12-13 20:27:47 +0100

    Modify format of timestamp returned by Get-Date / time

--- timestamp.ps1
+++ timestamp.ps1
@@ -2,14 +2,18 @@ function Get-Timestamp {
     [CmdletBinding()]
     Param()
 
-    Get-Date -Format "yyyy-MM-dd HH.mm.ss"
+    Get-Date -Format "yyyy-MM-dd_HH.mm.ss"
 
     <#
     .SYNOPSIS
         Returns the current date and time.
 
     .DESCRIPTION
-        Returns a timestamp of the current instant in the format "yyyy-MM-dd HH.mm.ss".
+        Returns a timestamp of the current instant 
+        suitable for filenames and logs with a precision 
+        at least to the second.
+        Note that the format may change in the future
+        and should not be relied upon.
 
     .ALIASES
         time
