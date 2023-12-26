3d8bddcf80b98f17ac439e2356d8230c64425483
Igor Akkerman <commit@igorakkerman.de>
2023-12-26 23:39:26 +0100

    Add Get-ProgramFiles function and programs alias

--- specialfolder.ps1
+++ specialfolder.ps1
@@ -1,3 +1,20 @@
+function Get-ProgramFilesFolder {
+    [CmdletBinding()]
+    Param()
+    
+    return [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::ProgramFiles)
+
+    <#
+    .SYNOPSIS
+        Returns the path to the Program Files folder.
+
+    .DESCRIPTION
+        Returns the path to the Program Files folder.
+
+    .ALIASES
+        progs
+    #>
+}
 function Get-MyDocumentsFolder {
     [CmdletBinding()]
     Param()
@@ -35,5 +52,6 @@ function Get-DesktopFolder {
     #>
 }
 
+New-Alias -Name programs -Value Get-ProgramFilesFolder -ErrorAction SilentlyContinue | Out-Null
 New-Alias -Name docs -Value Get-MyDocumentsFolder -ErrorAction SilentlyContinue | Out-Null
 New-Alias -Name desktop -Value Get-DesktopFolder -ErrorAction SilentlyContinue | Out-Null
